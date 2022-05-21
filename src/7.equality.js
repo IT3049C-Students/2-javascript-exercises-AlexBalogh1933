67f30a6651bad3ce793ec3b2909f49e34df1c572
Alex Balogh <baloghaj@mail.uc.edu>
2022-05-21 11:04:53 -0400

    finished test equality 2

--- src/7.equality.js
+++ src/7.equality.js
@@ -14,6 +14,12 @@ function strictEquality(val1, val2) {
 // Example: equality(1, 1) => true
 // Example: equality(1, false) => false
 function equality(val1, val2) {
+  if (val1 == val2){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = {
